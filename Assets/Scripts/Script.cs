a706f1a2baa1d1f685151f39de42153aa461d737
Matikuper <167240569+Matikuper@users.noreply.github.com>
2025-05-12 11:52:20 -0300

    Clonador terminado
    ACADEMIAAA

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clonarCubo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clonarCubo()
+    {
+        Instantiate(cubePrefab);
+    }
+}
